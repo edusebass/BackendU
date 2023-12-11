b54af385b36ddf36142aea9de7dc8c731fb8c102
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-11 14:46:20 -0500

    creacion de modelo de base y conexion basica

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const app = require('./server.js')
+const connection = require('./database.js')
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port ${app.get('port')}`);
+})
+
+
+
+
+connection()
