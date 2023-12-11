aba194683c034ca053af95f7ee39befb1ac2c649
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-11 15:31:34 -0500

    add hbs motor de plantillas

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //const MONGODB_URI = 'mongodb+srv://byrontosh:sistemas@cluster0.6e8zntc.mongodb.net/test'
-//const MONGODB_URI = 'mongodb://localhost:27017/portfolio'
+const MONGODB_URI = 'mongodb://localhost:27017/portfolio'
 connection = async()=>{
     try {
          await mongoose.connect(MONGODB_URI)
--- src/server.js
+++ src/server.js
@@ -1,26 +1,34 @@
-const express = require('express')
+const express = require('express');
 const path = require('path');
+const { engine } = require('express-handlebars');
 
 // Inicializaciones
-const app = express()
+const app = express();
 
 // Configuraciones 
-app.set('port',process.env.port || 1000)
-app.set('views',path.join(__dirname, 'views'))
+app.set('port', process.env.port || 1000);
+app.set('views', path.join(__dirname, 'views'));
 
 // Middlewares 
-app.use(express.urlencoded({extended:false}))
-
+app.use(express.urlencoded({ extended: false }));
 
 // Variables globales
 
 // Rutas 
-app.get('/',(req,res)=>{
-    res.send("Server on")
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 // Archivos estáticos
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuración del motor de plantillas Handlebars
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
-module.exports = app
+module.exports = app;
