26d1b85b231d72de68ce562c8952163a0bb4c00c
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-12 15:38:51 -0500

    app en funcionamiento hasta ruta login

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
+
 const app = require('./server.js')
 const connection = require('./database.js')
 
 app.listen(app.get('port'),()=>{
     console.log(`Server on port ${app.get('port')}`);
 })
-
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,35 +1,40 @@
-const express = require('express');
+const express = require('express')
 const path = require('path');
-const { engine } = require('express-handlebars');
+const { engine }  = require('express-handlebars')
 
 // Inicializaciones
-const app = express();
+const app = express()
 
 // Configuraciones 
-app.set('port', process.env.port || 1000);
+app.set('port',process.env.port || 1000)
+app.set('views',path.join(__dirname, 'views'))
 
 // Middlewares 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:false}))
 // Rutas 
 app.use(require('./routers/index.routes'))
+
 
 // Variables globales
 
 // Rutas 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 // Archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')))
 
-// Configuración del motor de plantillas Handlebars
-app.engine('.hbs', engine({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
+// Configuraciones 
 
-module.exports = app;
+app.set('views',path.join(__dirname, 'views'))
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
+
+
+module.exports = app
