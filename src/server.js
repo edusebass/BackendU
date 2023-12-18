d9fb41ca6731ceef5c9c4ee6aea791b60ad33585
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-12-18 15:52:18 -0500

    login

--- src/server.js
+++ src/server.js
@@ -2,19 +2,38 @@ const express = require('express')
 const path = require('path');
 const { engine }  = require('express-handlebars')
 const methodOverride = require('method-override');
+const session = require('express-session');
+const passport = require('passport');
 
 // Inicializaciones
 const app = express()
-
+require('./config/passport')
 // Configuraciones 
 app.set('port',process.env.port || 1000)
 app.set('views',path.join(__dirname, 'views'))
 
+
+
+
+
 // Middlewares 
 app.use(express.urlencoded({extended:false}))
 app.use(methodOverride('_method'))
+app.use(session({ 
+    secret: 'secret',
+    resave:true,
+    saveUninitialized:true
+}));
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Variables globales
+
+app.use((req,res,next)=>{
+    res.locals.user = req.user?.name || null
+    next()
+})
+
 
 // Rutas 
 app.get('/',(req,res)=>{
