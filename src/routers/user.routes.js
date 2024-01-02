a2572a275cf4596abb5ab9d9ddaef62e29700e52
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-01-02 16:18:03 -0500

    add routas

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,6 +1,6 @@
 const {Router} = require('express')
 const { redirectIfAuthenticated } = require('../helpers/validate-auth')
-const { renderRegisterForm, registerNewUser, renderLoginForm, loginUser, logoutUser } = require('../controllers/user.controller')
+const { renderRegisterForm, registerNewUser, renderLoginForm, loginUser, logoutUser, confirmEmail } = require('../controllers/user.controller')
 const router = Router()
 
 router.get('/user/register',redirectIfAuthenticated, renderRegisterForm)
@@ -8,6 +8,7 @@ router.post('/user/register',registerNewUser)
 
 router.get('/user/login', redirectIfAuthenticated, renderLoginForm)
 router.post('/user/login',loginUser)
+router.get('/user/confirmar/:token',confirmEmail)
 
 router.post('/user/logout',logoutUser)
 
